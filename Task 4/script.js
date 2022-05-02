cc84e83c551032ca8a40a735ebfe0ebbbc148807
Tomas <Tomas@DESKTOP-MI4J9VD>
2022-05-02 18:41:40 +0300

    Task 4 - done

--- Task 4/script.js
+++ Task 4/script.js
@@ -1,6 +1,6 @@
 /* ------------------------------ TASK 4 -----------------------------------
-Parašykite JS kodą, vartotjui atėjus į tinkaį kreipsis į cars.json failą
-ir iš atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+Parašykite JS kodą, vartotjui atėjus į tinkapį kreipsis į cars.json failą
+ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
 Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
 nurodomas gamintojas ir jo pagaminti modeliai.
 
@@ -10,3 +10,22 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const promise = fetch(ENDPOINT);
+promise
+	.then((resp) => resp.json())
+	.then((data) => {
+		data.forEach((cars) => {
+			const card = document.createElement('div');
+			card.classList.add('card');
+			const carBrand = document.createElement('h2');
+			carBrand.textContent = cars.brand;
+			const carModel = document.createElement('p');
+			carModel.textContent = cars.models;
+			card.append(carBrand, carModel);
+			output.append(card);
+		});
+
+		console.log(data);
+		// });
+	});
